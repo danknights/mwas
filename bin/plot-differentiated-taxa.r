df8d9acbf467e2407e21208bbc1091ee0a91db84
danknights <danknights@gmail.com>
2014-07-31 17:04:09 -0500

    Initial commit

---
+++ bin/plot-differentiated-taxa.r
@@ -0,0 +1,117 @@
+#!/usr/bin/env Rscript
+# Usage: run non-parametric test for differentiation according to metadata:
+#
+# run with Rscript make-beeswarm-plots.r -i taxontable -m map.txt -c column_name -o outdir
+# 
+# Usage: plot specific taxa (must match row name exactly from taxontable)
+# run with Rscript make-beeswarm-plots.r -i taxontable -w 'Bacteroides,Prevotella' -o outdir
+
+# REQUIRED GLOBAL VARIABLES: PLEASE EDIT
+source(paste(Sys.getenv('MWAS_DIR'),'/lib/gradients.r',sep=''))
+source(paste(Sys.getenv('MWAS_DIR'),'/lib/util.r',sep=''))
+source(paste(Sys.getenv('MWAS_DIR'),'/lib/stats.r',sep=''))
+
+library('RColorBrewer')
+library('optparse')
+library('vegan')
+library('beeswarm')
+
+# make option list and parse command line
+option_list <- list(
+    make_option(c("-i","--input_fp"), type="character",
+        help="QIIME-formatted input taxon table (tab-delimited text, not biom) [required]."),
+    make_option(c("-m","--map_fp"), type="character",default=NULL,
+        help="QIIME-formatted mapping file (optional). If provided, only samples in both taxon table and mapping file will be plotted."),
+    make_option(c("-c","--column"), type="character",default=NULL,
+        help="Name of metadata column to color plot by (optional). If included, does not plot gradients."),
+    make_option(c("-M","--min_prevalence"), type="numeric",default=.1,
+        help="Minimum fraction of samples in which taxon must be present to be included [default: %default]."),
+    make_option(c("-w", "--which_taxa"), type="character", default=NULL,
+        help="Comma-separated list of taxa to plot [default: plot top --nplot taxa]"),
+    make_option(c("-s", "--shorten_taxa"),action='store_true',default=FALSE,
+        help="Shorten taxonomy names to lowest defined level. [default: %default]"),
+    make_option(c("-n", "--nplot"), type="numeric", default=NULL,
+        help="Number of taxa to plot (in order of decreasing significance). Ignored if --which_taxa exists [default: %default]"),
+	make_option(c("-a","--alpha"), type='numeric', default=.05,
+		help='Maximum false discovery rate to report. Ignored if --which_taxa exists or --nplot exists [default: %default]'),
+    make_option(c("-o", "--outdir"), type="character", default='.',
+        help="Output directory [default %default]")
+)
+opts <- parse_args(OptionParser(option_list=option_list), 
+    args=commandArgs(trailing=TRUE))
+
+# create output directory if needed
+if(opts$outdir != ".") dir.create(opts$outdir,showWarnings=FALSE, recursive=TRUE)
+
+# LOAD DATA
+x <- t(read.table(opts$input_fp,sep='\t',head=T,row=1,check=F))
+if(!is.null(opts$map_fp)){
+	m <- read.table(opts$map_fp,sep='\t',head=T,row=1,check=F,comment='')
+	x <- x[intersect(rownames(x),rownames(m)),,drop=F]
+	m <- droplevels(m[rownames(x),,drop=F])
+}
+
+# remove rare features
+x <- x[,colMeans(x > 0) >= opts$min_prevalence]
+
+# asin sqrt
+x <- asin(sqrt(x))
+
+# check that taxon.names are in taxon table
+if(is.null(opts$which_taxa)){
+	taxon.names <- colnames(x)[rev(order(colMeans(x)))]
+	taxon.names <- taxon.names[1:min(opts$nplot, length(taxon.names))]
+} else {
+	taxon.names <- strsplit(opts$which_taxa,',')[[1]]
+
+	if(!all(taxon.names %in% colnames(x))){
+		stop(paste('The following taxa are not present in the taxon table:',
+					paste(taxon.names[!(taxon.names %in% colnames(x))],collapse=', '),
+					'\n'))
+	}
+} 
+if(!is.element(opts$column,colnames(m))) stop(paste(opts$column,'not in mapping file.'))
+
+# identify differentiated features
+diff.tests <- differentiation.test(x, m[,opts$column], parametric=FALSE)
+
+if(is.null(opts$which_taxa)){
+	if(is.null(opts$nplot)){
+		hit.ix <- which(diff.tests$qvalues <= opts$alpha)
+	} else {
+		hit.ix <- order(diff.tests$pvalues)[1:min(opts$nplot,length(diff.tests$pvalues))]
+	}
+} else {
+	hit.ix <- match(opts$which_taxa, colnames(x))
+}
+
+if(length(hit.ix) == 0){
+	stop('No hits found.\n')
+} else {
+	cat("There were",length(hit.ix),"taxa significant at FDR of",opts$alpha,'\n')
+}
+
+# save hits
+write.differentiation.test.results(diff.tests, filename=sprintf('%s/test_results.txt',opts$outdir))
+
+if(opts$shorten_taxa){
+	colnames(x) <- shorten.taxonomy(colnames(x))
+	taxon.names <- shorten.taxonomy(taxon.names)
+}
+		
+# plots
+env <- m[,opts$column]
+fp <- sprintf('%s/beeswarms.pdf',opts$outdir)
+pdf(fp,width=4,height=4)
+par(oma=c(4,0,0,0),mar=c(4,4,.5,.5))
+cols <- y.colors <- c(brewer.pal(9,'Set1'),brewer.pal(9,'Pastel1'),brewer.pal(8,'Dark2'),brewer.pal(8,'Accent'))[-6]
+cols <- sprintf('%saa',cols)
+for(i in hit.ix){
+	taxon <- x[,i]
+	taxon.name <- colnames(x)[i]
+	beeswarm(taxon ~ env,corral='random',las=2,
+		col='#00000099',
+		bg=cols,
+		pch=21,ylab=taxon.name,xlab='')
+}
+dev.off()
--- lib/stats.r
+++ lib/stats.r
@@ -1,3 +1,101 @@
+# returns p-values, q-values, 
+# indices of those below alpha, and renamed features with '*' etc.
+# and subset of data if requested
+# add.stars adds stars to the column names that are significant at .05, .01, .001, .0001
+# if filename is provided, saves result of statistical test to file
+# if parametric, uses t-test/ANOVA; else uses mann-whitney U/Kruskal-wallis
+"differentiation.test" <- function (x,category, alpha=0.05, parametric=TRUE,
+		include.subset=FALSE){
+	category <- as.factor(as.character(category))
+	if(length(unique(category)) < 2) stop('Category only has one level')
+	if(parametric){
+		pvals <- apply(x,2, function(taxon){
+				if(var(taxon) == 0){
+					NA
+				} else {
+					summary(lm(taxon~category))[[4]][2,4]
+				}
+			})
+		stats <- apply(x,2, function(taxon){
+				if(var(taxon) == 0){
+					NA
+				} else {
+					summary(lm(taxon~category))[[4]][2,1]
+				}
+			})
+	} else {
+		if(length(levels(category)) == 2){
+			ix1 <- category == levels(category)[1]
+			ix2 <- category == levels(category)[2]
+			pvals <- apply(x,2,function(taxon) wilcox.test(taxon[ix1],taxon[ix2],exact=FALSE)$p.value)
+			stats <- apply(x,2,function(taxon) wilcox.test(taxon[ix1],taxon[ix2],exact=FALSE)$statistic)
+		} else {
+			pvals <- apply(x,2,function(taxon) kruskal.test(taxon ~ category)$p.value)
+			stats <- apply(x,2,function(taxon) kruskal.test(taxon ~ category)$statistic)
+		}
+	}
+	na.ix <- is.na(pvals)
+	
+	adj.pvals <- rep(NA,length(pvals))
+	names(adj.pvals) <- names(pvals)
+	adj.pvals[!na.ix] <- p.adjust(pvals[!na.ix],'fdr')
+	keep.ix <- adj.pvals < alpha
+	keep.ix[is.na(keep.ix)] <- FALSE
+	if(!any(keep.ix)) stop('select.features failed to find any features.')
+
+	# add stars to column names based on significance
+	annotations <- colnames(x)
+	thresholds <- c(.05, .01, .001, .0001)
+	for(i in seq_along(thresholds)){
+		
+		star.ix <- adj.pvals[!na.ix] <= thresholds[i]
+		
+		if(any(star.ix)){
+			for(j in which(star.ix)){
+				annotations[!na.ix][j] <- paste(annotations[!na.ix][j],'*',sep='')
+			}
+		}
+	}
+	result <- list()
+	result$annotations <- annotations
+	result$features <- which(keep.ix)
+	result$qvalues <- adj.pvals
+	result$pvalues <- pvals
+	result$stats <- stats
+	
+	# classwise means
+	result$classwise.means <- t(apply(x,2,function(xx) sapply(split(xx,category),mean)))
+	colnames(result$classwise.means) <- sprintf('%s mean',colnames(result$classwise.means))
+	
+	if(include.subset){
+		result$subset <- x[,keep.ix,drop=F]
+		colnames(result$subset) <- annotations[keep.ix]
+	}
+	
+	
+	return(result)
+}
+
+
+# saves list of results from differentiation.test to file (or prints)
+"write.differentiation.test.results" <- function(results, filename='differentiated.features.txt'){
+	if(!is.null(filename)){
+		scipen.save <- options('scipen')
+		options(scipen=20)
+		hits <- cbind(results$pvalues, results$qvalues)
+		hits <- cbind(hits, results$classwise.means)
+		colnames(hits)[1:2] <- c('pvalue','qvalue')
+		hits <- hits[!is.na(hits[,1]),]
+		hits <- hits[order(hits[,1]),]
+		sink(filename)
+		cat('Feature\t')
+		write.table(hits,quote=F,sep='\t')
+		sink(NULL)
+		options(scipen=scipen.save)
+	}
+}
+
+
 # performs a linear test of association of predictors in X (e.g. clinical data)
 # with each column in Y (e.g. bacterial taxa).
 # includes option for mixed linear model with a random effect for e.g. multiple samples
