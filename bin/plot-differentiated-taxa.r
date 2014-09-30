2fc68827ebbac2468d35c50fce3e61eeb2eb50ae
GabeAl <GabeAl@users.noreply.github.com>
2014-09-30 15:18:43 -0500

    Update plot-differentiated-taxa.r
    Factored out the plotting of differentiated taxa (offloaded to /lib/visualize.R)

--- bin/plot-differentiated-taxa.r
+++ bin/plot-differentiated-taxa.r
@@ -70,7 +70,7 @@ if(!is.null(opts$map_fp)){
 }
 browser()
 
-# remove rare features
+# remove rare features (do by minimum prevalence or average prevalence)
 x <- x[,colMeans(x > 0) >= opts$min_prevalence]
 
 # data transform
@@ -144,21 +144,6 @@ if(!is.null(opts$category_order)){
 	env <- factor(env,levels=level.order)
 }
 
-cols <-  c(brewer.pal(9,'Set1'),brewer.pal(9,'Pastel1'),brewer.pal(8,'Dark2'),brewer.pal(8,'Accent'))[-6]
-cols[1:2] <- cols[2:1]
-cols <- sprintf('%sbb',cols)
-for(i in hit.ix){
-	taxon <- x[,i]
-	taxon.name <- colnames(x)[i]
-	filename.i <- sprintf('%s/beeswarm-%s.pdf',opts$outdir,taxon.name)
-	pdf(filename.i,width=8.5,height=11)
-	par(oma=rep(c(0,0),2),mar=c(12,8,8,3), cex.axis=.65, cex.lab=.65, cex=2)
-	cex.lab <- max(.45, 1 - strwidth(taxon.name, units='figure') / 4)
-	cex.lab <- .45
-	cex.axis <- .45
-	beeswarm(taxon ~ env,corral='random',las=2,
-		col='#000000bb',
-		bg=cols,
-		pch=21,ylab=taxon.name,xlab=opts$x_axis_label,cex.lab=cex.lab, cex.axis=cex.axis)
-	dev.off()
-}
+# need: cols, hit.ix, 
+# call with opts$outdir as outdir, opts$x_axis_label
+plot.beeswarm.dt(cols, opts$x_axis_label, hit.ix, env, opts$outdir)
