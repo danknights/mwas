bea123347c48f79d03895588f7ead30aacc6796e
hwangtiger <hwangtiger@gmail.com>
2014-10-10 01:43:26 -0500

    added test fiel for model.evaluate

---
+++ test/test.model.evaluation.R
@@ -0,0 +1,17 @@
+# Test model evaluation function
+# -------
+# Input: 
+#     x : feature vector (sample x features)
+#  model: trained model
+#     y : desired response
+#
+#--------
+# Function of model.evaluation
+
+"test.model.evaluation" <- function(x, model, y) {
+  #  
+  # 
+  # 
+  
+}
+
