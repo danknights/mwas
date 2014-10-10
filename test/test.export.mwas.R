1fe1d7a1d372abc457ae049147f73a762deae462
hwangtiger <hwangtiger@gmail.com>
2014-10-10 03:12:45 -0500

    added test file for export.mwas

---
+++ test/test.export.mwas.R
@@ -0,0 +1,19 @@
+# Export results to files
+#
+# --- input:
+#   trained.model : trained claissifier model and other parameters
+#        feat.set : selected feature vector index
+#     test.results: output from model.evaluation
+#    resutls.plot : parameters for plotting
+#            opts : options from keyboard
+#
+#------ ouput
+#     export the required results to the designated directory (opts$outdir)
+#
+"test.export.mwas" <- function(trained.model, feat.set, test.results, 
+                               results.plot, opts){
+  #
+  #
+  #
+  
+}
