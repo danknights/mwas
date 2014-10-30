9efb8f998ca7b4566a0e3b84944fa2aee6e27b2d
hwangtiger <hwangtiger@gmail.com>
2014-10-30 09:53:36 -0500

    add import function

---
+++ lib/import_params.R
@@ -0,0 +1,16 @@
+# Function: import parameters 
+#
+# Contributors: Hu
+# -----
+# input:
+#       opts : feature vector
+# 
+# output:
+#   param.list : a table of parameters that needed in each corresponding fucntion
+# 
+# ------
+#  Last update: 10/27/2014
+#
+"import.mwas" <- function(opts){
+  
+}
