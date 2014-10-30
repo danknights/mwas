da0274331993fd0a93d08d7144c19c62ed7a586e
hwangtiger <hwangtiger@gmail.com>
2014-10-30 14:41:22 -0500

    function updates

--- example/example_1.R
+++ example/example_1.R
@@ -5,26 +5,14 @@ source('~/Documents/R/mwas_git/lib/util.load.r')
 library(biom)
 setwd("~/Documents/R/mwas_git")
 
-opts <- list(make_option(c("-w", "--mode"),type='character',
-                    help="Function mode [required]"),
-make_option(c("-i","--OTU_fp"), type="character",
-            help="BIOM format or classic format of OTU table or other matrix [requried]"),
-make_option(c("-m","--map_fp"), type="character",
-            help="Mapping file  [required]."),
-make_option(c("-c","--category"), type="character",
-            help="Column name in the mapping file [requried]"),
-make_option(c("-t", "--method"),type='character',     #default="RF",
-            help="Classifier type [required for model training]"),
-make_option(c("-o", "--outdir"),type='character',default=".",
-            help="Output directory [default: %default]")
-)
-
+opts <- list()
 opts$mode <- "train"
 opts$OTU_fp <- "test/data/GG_100nt_even10k-adults-s20.biom"
 opts$map_fp <- "test/data/gg-map-adults.txt"
 opts$category <- "COUNTRY"
-opts$
+opts$outdir <- "example/"
 
+###################
 mapping <- load.qiime.mapping.file('test/data/gg-map-adults.txt')
 dim(mapping)
 #x <- read.table('test/data/GG_100nt_even10k-adults-biom',sep='\t',head=T,row=1,check=F)
@@ -38,3 +26,16 @@ response <- as.factor(mapping[,"COUNTRY"])
 source('~/Documents/R/mwas_git/lib/train.r')
 
 best.model <- train.mwas(feat.Data, response, is.feat = FALSE)
+###################
+opts <- list()
+opts$mode <- "train"
+opts$OTU_fp <- "test/data/GG_100nt_even10k-adults-s20.biom"
+opts$map_fp <- "test/data/gg-map-adults.txt"
+opts$category <- "COUNTRY"
+opts$outdir <- "example/"
+
+case.mode <- tolower(opts$mode)
+
+mwas.obj <- import.train.mwas(opts, type="train")
+train.mwas(mwas.obj)
+print("Training a model is finished!")
